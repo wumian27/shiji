9f7033fb04541bb6fa1bec7a1661d6c989324fb8
wumian27 <2461779663@qq.com>
2019-04-11 23:01:59 +0800

    555

---
+++ youji/server/routes/tupiao.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+let util = require('util');
+var formidable = require('formidable');
+let uuidv1 = require('uuid/v1');
+let path = require('path');
+let fs = require('fs')
+/* GET home page. */
+router.post('/', function(req, res, next) {
+// console.log(uuidv1());
+
+    var form = new formidable.IncomingForm();
+    form.uploadDir = "../server/public/images";
+    form.encoding = 'utf-8';
+    form.parse(req, function(err, fields, files) {
+        console.log(files);
+        
+        const png = path.extname(files.file.name);
+        const uuidiv = uuidv1();
+        let all = uuidiv + png;
+          
+        let old = files.file.path;
+       let n = '..\\server\\public\\images\\'+all;
+       fs.rename(old,n,(err,re)=>{
+           res.json({
+              path:all
+           })
+       })
+      
+    });
+    // res.json({
+    //     code:1
+    // })
+})
+
+module.exports = router;
--- youji/travel/src/component/center/center.js
+++ youji/travel/src/component/center/center.js
@@ -21,23 +21,23 @@ class Center extends Component {
                 address: "",
                 sex: ""
             },
-            sceneTicket:[],
-            airportTicket:[]
+            sceneTicket: [],
+            airportTicket: []
         }
     }
     componentDidMount() {
         document.documentElement.scrollTo(0, 0);
-        axios.post('http://localhost:3001/query',{
-            telephone:"18718180036"
+        axios.post('http://localhost:3001/query', {
+            telephone: "18718180036"
         }).then(res => {
             // console.log(res);
-            if(res.data.code){
+            if (res.data.code) {
                 this.setState({
-                    info:res.data.su[0]
+                    info: res.data.su[0]
                 })
-                
+
             }
-            
+
         })
     }
 
@@ -53,7 +53,7 @@ class Center extends Component {
     handleChange(e) {
         let sex = e.target.value
         this.setState({
-            info: {...this.state.info,sex}
+            info: { ...this.state.info, sex }
         })
     }
     submit() {
@@ -62,7 +62,7 @@ class Center extends Component {
             birthday = this.birthday.value,
             sex = this.state.info.sex,
             address = this.address.value;
-        if (!name || !nc || !birthday || !address||!sex) {
+        if (!name || !nc || !birthday || !address || !sex) {
             console.log('请完善信息');
         } else {
             axios.post('http://localhost:3001/update/person',
@@ -106,92 +106,106 @@ class Center extends Component {
         this.setState({
             show: flag
         });
-        if(arg==="travel"){
-            axios.post('http://localhost:3001/center/ticket/scene',{
-                telephone:"18718180036"
+        if (arg === "travel") {
+            axios.post('http://localhost:3001/center/ticket/scene', {
+                telephone: "18718180036"
             }).then(res => {
                 // console.log(res);
                 this.setState({
-                 sceneTicket:res.data.suc
+                    sceneTicket: res.data.suc
                 })
             })
         }
-       if(arg === 'airport'){
-        axios.post('http://localhost:3001/center/ticket/airport',{
-            telephone:"18718180036"
-        }).then(res => {
-            // console.log(res);
-            this.setState({
-             airportTicket:res.data.suc
+        if (arg === 'airport') {
+            axios.post('http://localhost:3001/center/ticket/airport', {
+                telephone: "18718180036"
+            }).then(res => {
+                // console.log(res);
+                this.setState({
+                    airportTicket: res.data.suc
+                })
             })
-        })
-       }
+        }
     }
     showInput(flag) {
         this.setState({
-            input: flag    
+            input: flag
         })
-        this.nc.value="";
-        this.name.value="";
-        this.birthday.value="";
-        this.state.info.sex="";
-        this.address.value="";
+        this.nc.value = "";
+        this.name.value = "";
+        this.birthday.value = "";
+        this.state.info.sex = "";
+        this.address.value = "";
     }
-    submitUpdate(){
-     let pwd = this.pwd.value,
-     newPwd = this.newPwd.value,
-     quePwd = this.quePwd.value;
-     if(!pwd||!newPwd||!quePwd){
-         console.log('请完善信息');
-     }else if(quePwd!==newPwd){
-       console.log('输入密码不一致');
-     }else{
-         axios.post('http://localhost:3001/update/pwd',{telephone:"18718180036",pwd,quePwd}).then(res=>{
-            if(res.data.code){
-                console.log('修改成功');
-            }else{
-                console.log('当前密码错误');
-                
-            }
-         })
-     }
-    
-    this.pwd.value="";
-    this.newPwd.value="";
-    this.quePwd.value=""
-        
+    submitUpdate() {
+        let pwd = this.pwd.value,
+            newPwd = this.newPwd.value,
+            quePwd = this.quePwd.value;
+        if (!pwd || !newPwd || !quePwd) {
+            console.log('请完善信息');
+        } else if (quePwd !== newPwd) {
+            console.log('输入密码不一致');
+        } else {
+            axios.post('http://localhost:3001/update/pwd', { telephone: "18718180036", pwd, quePwd }).then(res => {
+                if (res.data.code) {
+                    console.log('修改成功');
+                } else {
+                    console.log('当前密码错误');
+
+                }
+            })
+        }
+
+        this.pwd.value = "";
+        this.newPwd.value = "";
+        this.quePwd.value = ""
+
     }
 
-    cancelAirport(index,id){
+    cancelAirport(index, id) {
         let newArr = [...this.state.airportTicket];
-        newArr.splice(index,1);
+        newArr.splice(index, 1);
         this.setState({
-            airportTicket:newArr
+            airportTicket: newArr
         });
 
-        axios.post('http://localhost:3001/ticket/dele/airport',{
-            uid:"18718180036",
-            _id:id
-        }).then(res =>{
+        axios.post('http://localhost:3001/ticket/dele/airport', {
+            uid: "18718180036",
+            _id: id
+        }).then(res => {
             // console.log(res);
-            
+
         })
     }
-    cancelTicket(index,id){
+    cancelTicket(index, id) {
         let newArr = [...this.state.sceneTicket];
-        newArr.splice(index,1);
+        newArr.splice(index, 1);
         this.setState({
-            sceneTicket:newArr
+            sceneTicket: newArr
         });
 
-        axios.post('http://localhost:3001/ticket/dele/airport',{
-            uid:"18718180036",
-            _id:id
-        }).then(res =>{
+        axios.post('http://localhost:3001/ticket/dele/airport', {
+            uid: "18718180036",
+            _id: id
+        }).then(res => {
             // console.log(res);
-            
+
         })
     }
+     //图片提交
+    // tijiao(e){
+    //     e.preventDefault();
+    //     let formData = new FormData();
+    //     console.log(formData);
+    //     formData.append('file', this.state.file);
+    //     axios.post("http://localhost:3001/tu",formData, {headers: {
+    //       'Content-Type': 'multipart/form-data'}
+    //     }).then(res =>{
+    //       console.log(res)
+    //     })
+    //     console.log(22);
+        
+    //   }
     render() {
         return <div>
             <Head></Head>
@@ -203,6 +217,7 @@ class Center extends Component {
                         <li onClick={() => { this.select('ziliao') }}>个人资料</li>
                         <li onClick={() => { this.select('pwd') }}>密码设置</li>
                         <li>发表游记</li>
+                        <li onClick={() => { this.select('watch') }}>查看游记</li>
                     </ul>
                     <div className="center-right">
                         <div className={this.state.show.travel ? "" : "none"}>
@@ -215,27 +230,27 @@ class Center extends Component {
                                         <th>价格</th>
                                     </tr>
                                 </tbody>
-                                  {
-                                      this.state.sceneTicket.map((v,index )=>{
-                                          return  <tbody className="tbody" key={v._id}>
-                                          <tr>
-                                              <td>{v.scence}</td>
-                                              <td>{v.num}
-                                                 
-                                              </td>
-                                              <td>{v.time}</td>
-                                              <td className="show_price">￥{v.money}
-                                                <button 
-                                                onClick={()=>{this.cancelTicket(index,v._id)}}
-                                                className="buy_cancel"
-                                                >取消</button>
-                                              </td>
-                                          </tr>
-                                      </tbody>
-      
-                                      })
-                                  }
-                               
+                                {
+                                    this.state.sceneTicket.map((v, index) => {
+                                        return <tbody className="tbody" key={v._id}>
+                                            <tr>
+                                                <td>{v.scence}</td>
+                                                <td>{v.num}
+
+                                                </td>
+                                                <td>{v.time}</td>
+                                                <td className="show_price">￥{v.money}
+                                                    <button
+                                                        onClick={() => { this.cancelTicket(index, v._id) }}
+                                                        className="buy_cancel"
+                                                    >取消</button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+
+                                    })
+                                }
+
                             </table>
                         </div>
 
@@ -252,28 +267,28 @@ class Center extends Component {
                                     </tr>
                                 </tbody>
                                 {
-                                    this.state.airportTicket.map((v,index) =>{
+                                    this.state.airportTicket.map((v, index) => {
                                         return <tbody className="tbody" key={v._id} >
-                                        <tr>
-                                            <td>{v.start}</td>
-                                            <td>{this.formatTime(v.time)+" "+v.startTime}
+                                            <tr>
+                                                <td>{v.start}</td>
+                                                <td>{this.formatTime(v.time) + " " + v.startTime}
                                                     <p>{v.startAirport}</p>
-                                            </td>
-                                            <td>{this.formatTime(v.time)+" "+v.endTime}
+                                                </td>
+                                                <td>{this.formatTime(v.time) + " " + v.endTime}
                                                     <p>{v.endAirport}</p>
-                                            </td>
-                                            <td>{v.destination}</td>
-                                            <td>{v.num}</td>
-                                            <td className="show_price">￥{v.money} 
-                                            <button
-                                            onClick={()=>{this.cancelAirport(index,v._id)}} 
-                                            className="buy_cancel">取消</button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
+                                                </td>
+                                                <td>{v.destination}</td>
+                                                <td>{v.num}</td>
+                                                <td className="show_price">￥{v.money}
+                                                    <button
+                                                        onClick={() => { this.cancelAirport(index, v._id) }}
+                                                        className="buy_cancel">取消</button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
                                     })
                                 }
-                                
+
 
 
                             </table>
@@ -286,7 +301,7 @@ class Center extends Component {
                                     <span className={this.state.input ? "none" : "weishe"}>{this.state.info.nc ? this.state.info.nc : '未设置'}</span>
                                     <input
                                         ref={input => this.nc = input}
-                                      
+
                                         type="text"
                                         className={this.state.input ? "" : "none"} />
                                 </p>
@@ -295,7 +310,7 @@ class Center extends Component {
                                     <span className={this.state.input ? "none" : "weishe"}>{this.state.info.name ? this.state.info.name : '未设置'}</span>
                                     <input
                                         ref={input => this.name = input}
-                                     
+
                                         type="text"
                                         className={this.state.input ? "" : "none"} />
                                 </p>
@@ -339,7 +354,7 @@ class Center extends Component {
                                     <span className={this.state.input ? "none" : "weishe"}>{this.state.info.address ? this.state.info.address : '未设置'}</span>
                                     <input
                                         ref={input => this.address = input}
-                                      
+
                                         type="text"
                                         className={this.state.input ? "" : "none"} />
                                 </p>
@@ -354,7 +369,7 @@ class Center extends Component {
                         </div>
 
 
-                        <div className={this.state.show.pwd ? "" : "none"}>
+                        {/* <div className={this.state.show.pwd ? "" : "none"}>
                             <form className="center-form">
                                 <p>
                                     <label htmlFor="">当前密码:</label>
@@ -372,8 +387,42 @@ class Center extends Component {
                                     <span className="center-btn" onClick={()=>{this.submitUpdate()}} >修改</span>
                                 </p>
                             </form>
-                        </div>
+                        </div> */}
 
+
+                        <div className={this.state.show.pwd ? "" : "none"}>
+                            <div className="youji">
+                                <div className="bck">
+                                    <img src="/img/luxian/default_bg.png" className="bck-img" />
+                                    <input type="text" className="bck-input" placeholder="填写标题" />
+                                    <form className="bck-form" >
+                                        <img src="/img/luxian/set_head_pic.png" className="bck-file" />
+                                        <p className="bck-p"> 设置封面</p>
+                                        <input
+                                            className="file"
+                                            type="file"
+                                            name="upload"
+                                            multiple="multiple"
+                                            onChange={(e) => { this.file(e) }} />
+                                        {/* <button type="submit" onClick={(e) => { this.tijiao(e) }}>提交</button> */}
+                                    </form>
+                                </div>
+                                <div className="bck-bott">
+                                    <div class="ql-editor ql-blank" id="edit" data-gramm="false" contenteditable="true" placeholder="在这里添加旅游心得"></div>
+                                    <form className="bcktn-form" >
+                                        <img src="/img/luxian/set_head_pic.png" className="bck-file" />
+                                        <span>选择照片</span>
+                                        <input
+                                            className="btnfile"
+                                            type="file"
+                                            name="upload"
+                                            multiple="multiple"
+                                            onChange={(e) => { this.file(e) }} />
+                                        {/* <button type="submit" onClick={(e) => { this.tijiao(e) }}>提交</button> */}
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
